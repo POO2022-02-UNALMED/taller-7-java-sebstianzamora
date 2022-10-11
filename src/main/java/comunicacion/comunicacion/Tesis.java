dee57ed577c3e01963371643359bdcff8335d235
sebstianzamora <69611081+sebstianzamora@users.noreply.github.com>
2022-10-10 22:44:18 -0500

    :alien:

---
+++ src/main/java/comunicacion/comunicacion/Tesis.java
@@ -0,0 +1,73 @@
+package comunicacion;
+
+public class Tesis extends Escrito{
+    private String idea;
+    private String[] argumentos;
+    private String conclusion;
+    private String referencias;
+    private String interpretacion;
+
+    public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos, String conclusion, String referencias, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.idea = idea;
+        this.argumentos = argumentos;
+        this.conclusion = conclusion;
+        this.referencias = referencias;
+        this.interpretacion = interpretacion;
+    }
+
+    @Override
+    public int palabrasTotales(int palabrasPagina) {
+        return getPaginas()*5*palabrasPagina;
+    }
+
+    @Override
+    public String interpretacion() {
+        return interpretacion;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\n%s\n%s\n%d\n%s\n%d\n%s\n%s",getOrigen(),getTitulo(),getAutor(),getPaginas(),idea,argumentos.length,conclusion,referencias);
+    }
+
+    public String getIdea() {
+        return idea;
+    }
+
+    public void setIdea(String idea) {
+        this.idea = idea;
+    }
+
+    public String[] getArgumentos() {
+        return argumentos;
+    }
+
+    public void setArgumentos(String[] argumentos) {
+        this.argumentos = argumentos;
+    }
+
+    public String getConclusion() {
+        return conclusion;
+    }
+
+    public void setConclusion(String conclusion) {
+        this.conclusion = conclusion;
+    }
+
+    public String getReferencias() {
+        return referencias;
+    }
+
+    public void setReferencias(String referencias) {
+        this.referencias = referencias;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+}
